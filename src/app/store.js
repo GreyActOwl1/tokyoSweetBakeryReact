d09ddd4b74d97b76caca68d6aa8b2e38fabf6187
Delian Zidarov <d.zidarov@gmail.com>
2022-08-10 22:09:50 +0900

    prepull

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { shoppingCartReducer } from "../features/shoppingCart/shoppingCartSlice";
+import { shoppingCartReducer } from "../features/shoppingCart/shoppingCartSlice.js";
 
 export const store = configureStore({
   reducer: {
