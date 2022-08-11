22415ded068d6d9d877995c1d0bba3f782077b7d
Adrian Ea <aeas5454@outlook.com>
2022-08-11 00:19:44 -0400

    Add homepage hero and button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,30 @@
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Button } from "reactstrap";
 import FeaturedProductDisplay from "../components/FeaturedProductDisplay";
+import bakedGoodsDisplay from "../app/assets/img/yeh-xintong-unsplash-hero.jpg";
 
 const HomePage = () => {
   return (
     <Container fluid>
+      <Row className="parallax-background d-flex justify-content-center">
+        <Col xs="12" md="10" class="d-flex justify-content-center">
+          <div className="parallax-container mx-auto d-flex justify-content-center">
+            <h1 class="parallax">Tokyo Sweets Bakery</h1>
+            <img
+              src={bakedGoodsDisplay}
+              alt="baked goods display"
+              class="parallax parallax-img-background"
+            />
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="my-5 mx-0">
+        <Col className="">
+          <Button className="btn btn-secondary btn-lg">Shop Now </Button>
+          <hr/>
+        </Col>
+      </Row>
+
       <Row>
         <Col>
           <FeaturedProductDisplay />
