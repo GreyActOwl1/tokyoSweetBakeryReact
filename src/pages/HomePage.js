e6855bf6d29dd8598aaec75c06c37d17c3c922b0
Adrian Ea <aeas5454@outlook.com>
2022-08-11 23:34:19 -0400

    Add blank sliding display to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,28 @@
 import { Container, Row, Col, Button } from "reactstrap";
-import FeaturedProductDisplay from "../components/FeaturedProductDisplay";
 import StoreParallaxHero from "../components/StoreParallaxHero";
+import FeaturedProductsScrollingDisplay from "../components/FeaturedProductsScrollingDisplay";
+import FeaturedProductDisplay from "../components/FeaturedProductDisplay";
+
+
 
 const HomePage = () => {
   return (
     <Container className="p-0 mw-1600px mx-auto" fluid>
       <StoreParallaxHero />
-
+    
       <Row className="my-5 mx-auto">
         <Col className="">
           <Button className="btn btn-secondary btn-lg">Shop Now </Button>
           <hr />
-        </Col>
+        </Col>        
       </Row>
+      
       <Row className="my-5 mx-auto">
         <h2 className="mt-5 mb-3 text-center"> Featured Documents</h2>
       </Row>
+
+      <FeaturedProductsScrollingDisplay/>
+      
       <Row className="my-5 mx-auto">
         <Col>
           <FeaturedProductDisplay />
